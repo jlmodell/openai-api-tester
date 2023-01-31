ca8430a273910b8e2f384614abce128325193027
Jeff Modell <jmodell@local.busseinc.com>
2023-01-31 15:02:48 -0500

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,91 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [prompt, setPrompt] = useState("");
+  const [generatedResponse, setGeneratedResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const generateBio = async (e: any) => {
+    e.preventDefault();
+
+    setGeneratedResponse("");
+    setLoading(true);
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+    console.log("Edge function returned.");
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setGeneratedResponse((prev) => prev + chunkValue);
+    }
+
+    setLoading(false);
+    setPrompt("");
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-slate-200">
       <Head>
-        <title>Create Next App</title>
+        <title>Testing OpenAI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="flex w-full flex-1 flex-col items-center justify-around px-4 md:px-20 text-center mt-12">
+        <div className="flex w-full flex-col items-center justify-center">
+          <p className="text-2xl md:text-4xl font-bold">
+            Generate a response to a prompt
+          </p>
+          <textarea
+            rows={8}
+            className="w-full md:w-1/2 rounded-md border-gray-300 shadow-sm focus:border-0 focus:ring-0 my-5 bg-slate-700 focus:bg-slate-800 text-white p-4"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          <button
+            className="bg-slate-500 hover:bg-slate-600 text-white shadow-md px-4 py-2 rounded-md"
+            onClick={generateBio}
+            disabled={loading}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            {loading ? "Loading..." : "Generate Response"}
+          </button>
         </div>
+
+        {generatedResponse && (
+          <div className="mt-8 w-full md:w-2/3 bg-slate-700 text-white px-4 py-8 rounded-md">
+            <p className="text-xl font-bold">Generated Response</p>
+            <p className="text-xs md:text-md mt-4">{generatedResponse}</p>
+          </div>
+        )}
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
