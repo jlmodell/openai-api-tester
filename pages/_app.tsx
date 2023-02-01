ddcac981caa7742636d543af2808e3d784cd93ef
Jeff Modell <jmodell@local.busseinc.com>
2023-02-01 10:39:39 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,6 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-// const getBaseUrl = () => {
-//   if (typeof window !== "undefined") {
-//     return "";
-//   }
-//   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-//   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-// };
-
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
